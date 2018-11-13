9d7ec8c895172c99bc8c2aaf5d48b6259102eeb7
Caleb F <cfarn@CalebF.treva.local>
2018-11-13 10:21:31 -0500

    fixed dependencies

--- musicwithfriends/Program.cs
+++ musicwithfriends/Program.cs
@@ -19,7 +19,7 @@ namespace musicwithfriends
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-		.UseUrls("http://0.0.0.0:5000")
+		//.UseUrls("http://0.0.0.0:5000")
 		.UseStartup<Startup>();
     }
 }
